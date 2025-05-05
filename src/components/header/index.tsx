4670be188e062cfbfdc75e2f8b1643e363a32139
Paula-Stefany <paula.sfa@outlook.com>
2025-05-05 13:09:25 -0300

    header concluído

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,20 @@
 import { Outlet } from "react-router";
+import { FaBitcoin } from 'react-icons/fa';
 
 
 export function Header(){
     return(
         <>
-        <header>
-            CoinView
+        <header className="w-full py-10 flex justify-center text-amber-50">
+            <div className="flex items-center gap-2">
+                <FaBitcoin className="text-[32px] md:text-[40px]" />
+                <h1 className="text-3xl font-semibold md:text-4xl">CoinView</h1>
+
+            </div>
         </header>
-        <Outlet/>
+        <main className="w-full max-w-7xl m-auto  px-5">
+            <Outlet/>
+        </main>
         </>
     )
 }
