dca18fef5b6619484838249663a450d24692fa89
Paula-Stefany <paula.sfa@outlook.com>
2025-05-09 14:17:08 -0300

    Adicionando Link no header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router";
 import { FaBitcoin } from 'react-icons/fa';
+import { Link } from "react-router";
 
 
 export function Header(){
     return(
         <>
         <header className="w-full py-10 flex justify-center text-amber-50">
-            <div className="flex items-center gap-2">
-                <FaBitcoin className="text-[32px] md:text-[40px]" />
-                <h1 className="text-3xl font-semibold md:text-4xl">CoinView</h1>
+            <Link to={'/'}>
+                <div className="flex items-center gap-2">
+                    <FaBitcoin className="text-[32px] md:text-[40px]" />
+                    <h1 className="text-3xl font-semibold md:text-4xl">CoinView</h1>
 
-            </div>
+                </div>
+            </Link>
         </header>
         <main className="w-full max-w-7xl m-auto  px-5">
             <Outlet/>
